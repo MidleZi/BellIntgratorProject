e787bec9c2e6e9ddcd1432a4085457eaed8b48a5
MidleZi <MidleZi@users.noreply.github.com>
2018-03-06 16:21:46 +0300

    first commit

--- MyProject/src/main/java/ru/bellintegrator/myProject/organization/service/impl/OrganizationServiceImpl.java
+++ MyProject/src/main/java/ru/bellintegrator/myProject/organization/service/impl/OrganizationServiceImpl.java
@@ -1,6 +1,58 @@
 package ru.bellintegrator.myProject.organization.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import ru.bellintegrator.myProject.organization.service.OrganizationService;
+import ru.bellintegrator.myProject.organization.dao.OrganizationDAO;
+import ru.bellintegrator.myProject.organization.model.Organization;
+import ru.bellintegrator.myProject.organization.service.impl.OrganizationServiceImpl;
+import ru.bellintegrator.myProject.organization.view.OrganizationView;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class OrganizationServiceImpl implements OrganizationService {
+
+
+    private final Logger log = LoggerFactory.getLogger(OrganizationServiceImpl.class);
+
+    private final OrganizationDAO dao;
+
+    @Autowired
+    public OrganizationServiceImpl(OrganizationDAO dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    @Transactional
+    public void add(OrganizationView view) {
+        Organization users = new Organization(view.name, view.inn);
+        dao.save(users);
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<OrganizationView> organizations() {
+        List<Organization> all = dao.all();
+
+        Function<Organization, OrganizationView> mapOrganization = o -> {
+            OrganizationView view = new OrganizationView();
+            view.id = String.valueOf(o.getId());
+            view.name = o.getName();
+            view.name = o.getInn();
+
+            log.info(view.toString());
+
+            return view;
+        };
+
+        return all.stream()
+                .map(mapOrganization)
+                .collect(Collectors.toList());
+    }
+
 }
