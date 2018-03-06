73f079d67ebc4bd2dcbd99f1ae781bc28698d20f
MidleZi <MidleZi@users.noreply.github.com>
2018-03-06 16:35:10 +0300

    first commit

--- MyProject/src/main/java/ru/bellintegrator/myProject/users/controller/UsersController.java
+++ MyProject/src/main/java/ru/bellintegrator/myProject/users/controller/UsersController.java
@@ -1,4 +1,21 @@
 package ru.bellintegrator.myProject.users.controller;
 
-public class UsersController {
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.bellintegrator.myProject.users.view.UsersView;
+
+import java.util.List;
+
+public interface UsersController {
+
+    /**
+     * Add users
+     * @param users
+     */
+    void users(@RequestBody UsersView users);
+
+    /**
+     * Get all user
+     * @return JSON user value
+     */
+    List<UsersView> user();
 }
