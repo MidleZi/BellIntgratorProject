ec608eb023fc49408334e847d2de67697c2121a9
MidleZi <36714422+MidleZi@users.noreply.github.com>
2018-03-16 09:17:57 +0300

    Вспомнил, добавил анотации контроллера

--- MyProject/src/main/java/ru/bellintegrator/myproject/office/controller/impl/OfficeControllerImpl.java
+++ MyProject/src/main/java/ru/bellintegrator/myproject/office/controller/impl/OfficeControllerImpl.java
@@ -6,15 +6,19 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.bellintegrator.myproject.office.controller.OfficeController;
 import ru.bellintegrator.myproject.office.service.OfficeService;
 import ru.bellintegrator.myproject.office.view.OfficeView;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@RestController
+@RequestMapping(value = "/", produces = APPLICATION_JSON_VALUE)
 public class OfficeControllerImpl implements OfficeController {
 
     private final OfficeService officeService;
