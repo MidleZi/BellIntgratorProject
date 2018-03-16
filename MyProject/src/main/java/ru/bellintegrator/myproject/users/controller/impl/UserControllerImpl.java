1ce2667aaf6c8ef18f030efb17922f510aa2ebb0
MidleZi <36714422+MidleZi@users.noreply.github.com>
2018-03-16 09:18:41 +0300

    Добавил антоацию контроллера

--- MyProject/src/main/java/ru/bellintegrator/myproject/users/controller/impl/UserControllerImpl.java
+++ MyProject/src/main/java/ru/bellintegrator/myproject/users/controller/impl/UserControllerImpl.java
@@ -6,15 +6,19 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.bellintegrator.myproject.users.controller.UsersController;
 import ru.bellintegrator.myproject.users.service.UsersService;
 import ru.bellintegrator.myproject.users.view.UsersView;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@RestController
+@RequestMapping(value = "/", produces = APPLICATION_JSON_VALUE)
 public class UserControllerImpl implements UsersController {
 
     private final UsersService usersService;
