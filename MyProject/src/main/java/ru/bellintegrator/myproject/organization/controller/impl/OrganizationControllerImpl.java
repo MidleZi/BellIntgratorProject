f9b239bc09d0ed9e0e82dcfd5c7b6d455c15f2fd
MidleZi <36714422+MidleZi@users.noreply.github.com>
2018-03-16 09:16:46 +0300

    Не помню что делал и почему не закомитил

--- MyProject/src/main/java/ru/bellintegrator/myproject/organization/controller/impl/OrganizationControllerImpl.java
+++ MyProject/src/main/java/ru/bellintegrator/myproject/organization/controller/impl/OrganizationControllerImpl.java
@@ -6,15 +6,19 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.bellintegrator.myproject.organization.service.OrganizationService;
 import ru.bellintegrator.myproject.organization.view.OrganizationView;
 import ru.bellintegrator.myproject.organization.controller.OrganizationController;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@RestController
+@RequestMapping(value = "/", produces = APPLICATION_JSON_VALUE)
 public class OrganizationControllerImpl implements OrganizationController {
 
     private final OrganizationService organizationService;
